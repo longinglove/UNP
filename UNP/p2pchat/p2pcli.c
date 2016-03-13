ff427d502b6523ba53738de968d465d472d572b2
duqinglong <du_303412@163.com>
2016-03-13 18:14:26 +0800

    my minichat code

---
+++ UNP/p2pchat/p2pcli.c
@@ -0,0 +1,64 @@
+/*************************************************************************
+	> File Name: p2pcli.c
+	> Author: duqinglong
+	> Mail: du_303412@163.com 
+	> Created Time: 2016年03月13日 星期日 16时04分55秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<errno.h>
+
+#define BUFSIZE 1024
+#define ERROR(m)	\
+	do {perror(m); exit(EXIT_FAILURE);} while (0)
+
+int main()
+{
+	char recvbuf[BUFSIZE] = {0}, sendbuf[BUFSIZE] = {0};
+	int sock, ret;
+	struct sockaddr_in srvaddr;
+	pid_t pid;
+	if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		ERROR("sock error");
+	memset(&srvaddr, 0, sizeof(srvaddr));
+	srvaddr.sin_family = AF_INET;
+	srvaddr.sin_port = htons(4321);
+	srvaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	if (connect(sock, (struct sockaddr*)&srvaddr, sizeof(srvaddr)) < 0)
+		ERROR("connect error");
+	if ((pid = fork()) < 0)
+		ERROR("fork error");
+	if (pid == 0)
+	{
+		while (1)
+		{
+			memset(recvbuf, 0, sizeof(recvbuf));
+			if ((ret = read(sock, recvbuf, BUFSIZE)) < 0)
+				ERROR("read error");
+			else if (ret == 0)
+			{
+				printf("peer close\n");
+				break;
+			}
+			else
+				printf("%s", recvbuf);
+		}
+	}
+	else
+	{
+		while (fgets(sendbuf, BUFSIZE, stdin) != NULL)
+		{
+			write(sock, sendbuf, strlen(sendbuf));
+			printf("%s", recvbuf);
+			memset(sendbuf, 0, BUFSIZE);
+		}
+	}
+	return 0;
+}
---
+++ UNP/p2pchat/p2psrv.c
@@ -0,0 +1,73 @@
+/*************************************************************************
+	> File Name: p2psrv.c
+	> Author: duqinglong
+	> Mail: du_303412@163.com 
+	> Created Time: 2016年03月13日 星期日 16时12分49秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+#define BUFSIZE 1024
+#define ERROR(m)	\
+	do {perror(m); exit(EXIT_FAILURE);} while (0)
+
+int main()
+{
+	char rcvbuf[BUFSIZE];
+	int sock, conn, on = 1, ret;
+	struct sockaddr_in srvaddr, peeraddr;
+	socklen_t peerlen = sizeof(peeraddr);
+	pid_t pid;
+	if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+		ERROR("socket error");
+	memset(&srvaddr, 0, sizeof(srvaddr));
+	srvaddr.sin_family = AF_INET;
+	srvaddr.sin_port = htons(4321);
+	srvaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+		ERROR("setsockopt error");
+	if (bind(sock, (struct sockaddr*)&srvaddr, sizeof(srvaddr)) < 0)
+		ERROR("bind error");
+	if (listen(sock, SOMAXCONN) < 0)
+		ERROR("listen error");
+	if ((conn = accept(sock, (struct sockaddr*)&peeraddr, &peerlen)) < 0)
+		ERROR("accept error");
+	printf("id = %s, port = %d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
+	if ((pid = fork()) == -1)
+		ERROR("fork error");
+	if (pid == 0)
+	{
+		char sendbuf[BUFSIZE] = {0};
+		while (fgets(sendbuf, BUFSIZE, stdin) != NULL)
+		{
+			write(conn, sendbuf, strlen(sendbuf));
+			memset(sendbuf, 0, BUFSIZE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		while (1)
+		{
+			memset(rcvbuf, 0, BUFSIZE);
+			if ((ret = read(conn, rcvbuf, BUFSIZE)) == -1)
+				ERROR("read error");
+			if (ret == 0)
+			{
+				printf("peer close\n");
+				break;
+			}
+			printf("%s", rcvbuf);
+		}
+		exit(EXIT_SUCCESS);
+	}
+	return 0;
+}
